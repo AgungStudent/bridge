a863f67ecc06650e0fe23a8018603b1f4cc0c142
faizal00 <672021233@student.uksw.edu>
2024-07-31 10:43:42 +0700

    update templates : user

--- main-fe.py
+++ main-fe.py
@@ -12,6 +12,10 @@ def mitra_stock():
 def mitra_branch():
     return render_template("/mitra/mitra-branch.html")
 
+@app.route("/mitra/history")
+def mitra_history():
+    return render_template("/mitra/mitra-history.html")
+
 @app.route("/mitra/sign-in")
 def mitra_signin():
     return render_template("/mitra/mitra-signin.html")
@@ -19,6 +23,24 @@ def mitra_signin():
 @app.route("/mitra/sign-up")
 def mitra_signup():
     return render_template("/mitra/mitra-signup.html")
+
+#=====USER=====
+
+@app.route("/user/history")
+def user_history():
+    return render_template("/user/user-history.html")
+
+@app.route("/user/mitra")
+def user_mitra():
+    return render_template("/user/user-mitra.html")
+
+@app.route("/user/sign-up")
+def user_signup():
+    return render_template("/user/user-signup.html")
+
+@app.route("/user/sign-in")
+def user_signin():
+    return render_template("/user/user-signin.html")
 
 if __name__ == "__main__":
     print("from main")
