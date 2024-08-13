535fbbc31eeff654eef1dc7b1b1f5e79f8426baa
AgungPN <agungpn33@gmail.com>
2024-08-13 22:30:37 +0700

    feat(config): change mongo URI value

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
-MONGO_URI = "mongodb+srv://672021077:672021077@learn-mongo.bxhbc4i.mongodb.net/"
-# local: mongodb://localhost:27017
+MONGO_URI = "mongodb://localhost:27017"
 DATABASE_NAME = "food-bridge"
 USER_COLLECTION = "users"
 MITRA_COLLECTION = "mitras"
